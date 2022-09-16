5a61134882a0c050dbfa34d188cc7d685e738675
Julius Walther <julius.walther@sinnerschrader.com>
2022-09-16 10:39:35 +0200

    refactor(ts-cjs-esm-package): improve compatibility layer and build output structure

--- packages/ts-cjs-esm-package/src/index.ts
+++ packages/ts-cjs-esm-package/src/index.ts
@@ -2,6 +2,6 @@
  * Relative imports will be transformed using a post-build step using the `tsc-esm-fix` npm package.
  * This transform will only be run for the ESM output of our library.
  */
-export * from './deep/index'
+export * from './deep'
 
 export const Main = 'Main'
